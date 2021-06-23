abc087df47af312dc06336d54531d21bc14e39a1
Jeff Buda <budajeff@gmail.com>
2021-06-23 19:42:49 -0400

    hello world deploys and serves on AWS

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using aspnetcoreapp;
+
+namespace portfolio_backend_dotnet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseContentRoot(Directory.GetCurrentDirectory())
+              .UseIISIntegration()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+    }
+}
