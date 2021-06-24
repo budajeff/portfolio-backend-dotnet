068f3265557fabac6a329ac37012e7c3310be07e
Jeff Buda <budajeff@gmail.com>
2021-06-23 22:06:18 -0400

    added site-data endpoint

---
+++ Controllers/SiteContentController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Reflection;
+using System.Text.Encodings.Web;
+
+namespace portfolio_backend_dotnet
+{
+    public class SiteDataController : Controller
+    {
+        [Route("site-data")]
+        public string GetSiteData()
+        {
+            // an S3 bucket would be more appropriate but I'm using this as a trivial example
+            // of a dotnet core controller.
+            
+            var assembly = typeof(portfolio_backend_dotnet.SiteDataController).GetTypeInfo().Assembly;
+            using Stream resource = assembly.GetManifestResourceStream("portfolio_backend_dotnet.json.SiteData.json");
+            using StreamReader reader = new StreamReader(resource);
+            return reader.ReadToEnd();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,16 +2,22 @@ using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace aspnetcoreapp
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(context =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                return context.Response.WriteAsync("Hello from ASP.NET Core!");
+                endpoints.MapControllers();
             });
         }
     }
